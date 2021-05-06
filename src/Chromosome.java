91fb7184b1db83c9def324eac95c87d5a95fbeb8
Mohammad reza Abbasniya <53470224+mohammadAbbasniya@users.noreply.github.com>
2021-05-06 12:49:08 +0430

    Add files via upload

---
+++ src/Chromosome.java
@@ -0,0 +1,41 @@
+package src;
+
+/**
+ * class to represent a chromosome
+ * @param <GEN> data type of genomes that this chromosome will contains
+ */
+public class Chromosome<GEN> {
+    private final GEN[] genomes;
+
+    /**
+     * constructor of chromosome
+     * @param gens genomes that make up chromosome
+     */
+    public Chromosome(GEN...gens) {
+        this.genomes = gens;
+    }
+
+    /**
+     * @return genomes as an array
+     */
+    public GEN[] asArray() {
+        return genomes;
+    }
+
+    /**
+     * @param i : index of considered gen
+     * @return get i-th gen in chromosome
+     */
+    public GEN get(int i){
+        return genomes[i];
+    }
+
+    /**
+     * set a new value to a gen of chromosome
+     * @param i      : index of considered gen
+     * @param newVal : new value for that gen
+     */
+    public void set(int i, GEN newVal){
+        genomes[i] = newVal;
+    }
+}
